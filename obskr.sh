6457dc367e24237817f0b8be945743b11c419de1
Nicolas James <nicolas.james@gmail.com>
2011-08-17 12:42:33 +0200

    refactoring

--- obskr.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-icon=/home/nicolas/.config/openbox/obskr/openbox-logo-3.png
-keyboard_shortcuts=`cat /home/nicolas/.config/openbox/obskr/keyboard_shortcuts`
-notify-send --icon="$icon" "Openbox keyboard shortcuts" "$keyboard_shortcuts"
